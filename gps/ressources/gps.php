5f8f8b7bc02400186d1adea8fc5f1a0f0233ac91
Dominique Cavailhez <dominique@c92.fr>
2024-01-02 15:24:03 +0100

    OK gps avec couches WRI

--- gps/ressources/gps.php
+++ gps/ressources/gps.php
@@ -4,34 +4,40 @@ header("Pragma: no-cache");
 
 error_reporting(E_ALL);
 ini_set("display_errors", "on");
+//TODO click on new version button reloads
 
-// Default config
+// Default config.php
 $myol_rep = "../dist/";
 $gpx_rep = "";
 $map_keys = [];
+$vector_layers = [];
+$js_include = [];
+$html_include = [];
 
 // Get specific config
 if (file_exists("config.php")) {
-    include "config.php";
+  include "config.php";
 }
 
 // Calculate the last modification date of files
 $dependencies = array_unique(
-    glob("{*,*/*,$myol_rep*,$gpx_rep*}", GLOB_BRACE),
-    SORT_STRING
+  glob("{*,*/*,$myol_rep*,$gpx_rep*}", GLOB_BRACE),
+  SORT_STRING
 );
 date_default_timezone_set("Europe/Paris");
 $last_change_time = 0;
 foreach ($dependencies as $f) {
-    if (is_file($f)) {
-        $last_change_time = max($last_change_time, filemtime($f));
-    }
+  if (is_file($f)) {
+    $last_change_time = max($last_change_time, filemtime($f));
+  }
 }
 $last_change_date = date("Y-m-d H:i:s", $last_change_time);
 
+// Values used in javascript code
 $js_vars = json_encode([
-    "lastChangeDate" => $last_change_date,
-    "mapKeys" => $map_keys,
-    "gpxFiles" => glob($gpx_rep . "*.gpx", GLOB_BRACE),
-    "myolFiles" => glob($myol_rep . "myol.*s"),
+  "lastChangeDate" => $last_change_date,
+  "mapKeys" => $map_keys,
+  "gpxFiles" => glob($gpx_rep . "*.gpx", GLOB_BRACE),
+  "myolFiles" => glob($myol_rep . "myol.*s"),
+  "vectorLayers" => $vector_layers,
 ]);
